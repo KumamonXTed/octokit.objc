30e90d0c821df2a642a426ac02aad667fe2ec00f
AwayQuEM <awayquym@gmail.com>
2019-03-16 00:44:51 +0800

    add orm

--- OctoKit/OCTRepository.h
+++ OctoKit/OCTRepository.h
@@ -33,7 +33,7 @@
 @property (nonatomic, copy, readonly) NSString *language;
 
 // Whether this repository is private to the owner.
-@property (nonatomic, assign, getter = isPrivate, readonly) BOOL private;
+@property (nonatomic, assign, getter = isPrivate, readonly) BOOL privateBool;
 
 // Whether this repository is a fork of another repository.
 @property (nonatomic, assign, getter = isFork, readonly) BOOL fork;
